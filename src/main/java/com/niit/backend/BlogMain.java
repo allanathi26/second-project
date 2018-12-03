cbd4e0816068eab21b2cedf1ab123bb066f1404d
allanathi26 <43332514+allanathi26@users.noreply.github.com>
2018-12-03 09:34:15 +0530

    Add files via upload

---
+++ src/main/java/com/niit/backend/BlogMain.java
@@ -0,0 +1,50 @@
+package com.niit.backend;
+
+import java.util.Scanner;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import com.niit.config.DBConfig;
+import com.niit.dao.BlogPostDAO;
+import com.niit.dao.UserDAO;
+import com.niit.model.BlogPost;
+import com.niit.model.User;
+
+public class BlogMain 
+{
+public void blogOut()
+{
+	AbstractApplicationContext context = new AnnotationConfigApplicationContext(DBConfig.class);
+  	 BlogPostDAO blogPostDAO = (BlogPostDAO) context.getBean("blogpostDAO");
+  	 
+      
+      BlogPost blogPost=new BlogPost();
+      Scanner sc = new Scanner(System.in);
+      
+      System.out.println("Please select a category to do the manipulation");
+		System.out.println("1.Add Blog");
+		
+		int choice = sc.nextInt();
+		
+		switch(choice){
+		case 1:
+			System.out.println("Please Enter the user details to enter");
+			
+			System.out.println("Blog Title");
+			blogPost.setBlogTitle(sc.next());
+			
+			System.out.println("Blog Content");
+			blogPost.setBlogContent(sc.next());
+			
+			blogPostDAO.addBlogPost(blogPost);
+		
+			break;	
+		 default: System.out.println("Please enter a valid input");
+			
+		}	
+			context.close();
+			sc.close();
+
+}
+}
